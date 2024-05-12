64e2e5c28a833b5298cc4008d4c31f27da518fa4
Eric Streeper <eric.streeper@gmail.com>
2024-05-12 15:23:14 -0600

    Keep React stuff separate

--- lib/Parser.ts
+++ lib/Parser.ts
@@ -1,3 +1,6 @@
+type Record = string[]
+type ExtractedRecord = [Record, number];
+
 export function parse(dbFile: File) {
   const reader = new FileReader();
   reader.onload = (evt) => {
@@ -5,13 +8,14 @@ export function parse(dbFile: File) {
     if (typeof reader.result === 'string') {
       dbString = reader.result;
       // Windows uses carriage returns
-      const lines = dbString.replace('\r', '').split('\n');
+      const lines = dbString.replaceAll('\r', '').split('\n');
       const individuals = new Map();
       const families = new Map();
+      const records = new Map();
 
       let currentLineCount = 0;
-      //while (currentLineCount < lines.length) {
-      while (currentLineCount < 1000) {
+      while (currentLineCount < lines.length) {
+      //while (currentLineCount < 1000) {
         if (currentLineCount % 20000 === 0) {
           console.log(`On line ${currentLineCount}`);
         }
@@ -21,16 +25,14 @@ export function parse(dbFile: File) {
         if (isDelimiterLine(currentLine)) {
           const ret = extractRecord(lines, currentLineCount);
           const [record, newHeadPos] = extractRecord(lines, currentLineCount);
-          const recordType = getRecordType(record[0]);
-          if (recordType === 'INDIVIDUAL') {
-            individuals.set(keyName, record);
-          }
+          const recordID = getRecordID(record);
+	  records.set(recordID, record)
           currentLineCount = newHeadPos;
         } else {
           currentLineCount += 1;
         }
       }
-      console.log(individuals);
+      console.log(records);
     }
   }
   reader.readAsText(dbFile);
@@ -43,28 +45,18 @@ function isDelimiterLine(line: string) {
 }
 
 //type RecordType = 'FAMILY' | 'INDIVIDUAL' | 'SOURCE' | 'SUBM';
-function getRecordType(line: string) {
-  const individual_regex = /\d\s@I\d+@/;
-  const family_regex = /\d\s@F\d+@/;
-  const source_regex = /\d\s@S\d+@/;
-  const subm_regex = /\d\s@S\d+@/;
-  const individual_match = individual_regex.exec(line);
-  const family_match = family_regex.exec(line);
-  const source_match = source_regex.exec(line);
-  const subm_match = subm_regex.exec(line);
-
-  if (individual_match) {
-    return 'FAMILY';
-  } else if (family_match) {
-    return 'INDIVIDUAL';
-  } else if (source_match) {
-    return 'SOURCE';
-  } else if (subm_match) {
-    return 'SUBM';
+function getRecordID(record: Record): string {
+  const line = record[0];
+  const regex = /@((I|F|S|SUB)\d+)@/;
+  const match = line.match(regex);
+  if (match) {
+    return match[1]
+  } else {
+    throw new Error("Record does not contain an ID")
   }
 }
 
-function extractRecord(lines: Array<string>, startLine: number) {
+function extractRecord(lines: Array<string>, startLine: number): ExtractedRecord {
   let headPos: number = startLine;
   const rows = [];
   while (true) {
